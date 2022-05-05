70627a751d18009a09831ce337f3d314d6e06294
bl.assefa@jacobs-university.de <blenassefa00@gmail.com>
2022-05-05 14:43:38 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MomenTUM',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
