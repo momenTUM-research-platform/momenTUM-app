17830a053a3bdffa3f7a4e4a6048488f524531d0
Marco Ma <marcotxma@gmail.com>
2022-06-12 10:43:33 +0200

    pvt page created

---
+++ src/app/pvt/pvt.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pvt',
+  templateUrl: './pvt.page.html',
+  styleUrls: ['./pvt.page.scss'],
+})
+export class PvtPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
