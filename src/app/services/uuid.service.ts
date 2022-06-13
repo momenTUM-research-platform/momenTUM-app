7bdd8aab8fe4b6b24249b09f62f0e87cfabb1346
Constantin Goeldel <constantin.goeldel@tum.de>
2022-06-13 18:09:09 +0200

    propose changes to id generation

--- src/app/services/uuid.service.ts
+++ src/app/services/uuid.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UuidService {
-
-  constructor() { }
-
+  constructor() {}
+  // Math.random is not cryptographically secure, problably good enough for our purposes but the crypto library has a provably secure random number generator. See https://github.com/ai/nanoid#security
+  // With 25 ** 8 charakters, there are 152 587 890 625 possible values, which according to https://zelark.github.io/nano-id-cc/ comes out to ~2 days needed, in order to have a 1% probability of at least one collision, when creating ~ 1000 ids per hour, which is not unreasonable for our purposes.
   // implementation taken from https://www.fiznool.com/blog/2014/11/16/short-id-generation-in-javascript/
   generateUUID(prefix) {
-    const ALPHABET = '23456789ABDEGJKMNPQRVWXYZ'
-    const ID_LENGTH = 8
+    const ALPHABET = '23456789ABDEGJKMNPQRVWXYZ';
+    const ID_LENGTH = 8;
 
-    let rtn = ''
+    let rtn = '';
     for (let i = 0; i < ID_LENGTH; i++) {
-      rtn += ALPHABET.charAt(Math.floor(Math.random() * ALPHABET.length))
+      rtn += ALPHABET.charAt(Math.floor(Math.random() * ALPHABET.length));
     }
-    return prefix + rtn
+    return prefix + rtn;
   }
 }
