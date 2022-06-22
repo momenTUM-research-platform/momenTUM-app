3c363abc8d9bbeafbaa9145acb1cf01d5088e3e8
Blen <blenassefa00@gmail.com>
2022-06-22 11:28:08 +0200

    pvt testbed working

--- src/app/pvt/pvt.page.spec.ts
+++ src/app/pvt/pvt.page.spec.ts
@@ -1,22 +1,40 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { PvtPage } from './pvt.page';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { ChildrenOutletContexts } from '@angular/router';
+
+import { HTTP } from '@ionic-native/http/ngx';
+import { Storage } from '@ionic/storage-angular';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('PvtPage', () => {
   let component: PvtPage;
   let fixture: ComponentFixture<PvtPage>;
 
-  beforeEach(waitForAsync(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ PvtPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        RouterTestingModule,
+        IonicModule.forRoot(),
+
+      ],
+      providers: [
+        Storage,
+        HttpClient,
+        HttpHandler,
+        HTTP,
+        ChildrenOutletContexts,
+      ],
     }).compileComponents();
+
 
     fixture = TestBed.createComponent(PvtPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
